91f8f98950369dba0863d52acd03daf9f83e54f1
renggadiansa <renggadiansa@gmail.com>
2023-02-18 12:15:12 +0700

    note commit

---
+++ ai/main.py
@@ -0,0 +1,17 @@
+#print string
+a = "5"
+b = "5"
+print(a + b)
+
+
+#print number
+a = 5
+b = 5
+print(a + b)
+
+#string manipulation
+
+nama = "Rizky"
+str = "hello " + nama
+str = f"hello {nama}"
+print(str)
